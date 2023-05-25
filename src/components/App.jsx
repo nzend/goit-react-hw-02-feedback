133e8fe4db74d444537578a0eae938dbb7d1ddbf
nzend <115723110+nzend@users.noreply.github.com>
2023-05-25 22:17:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
+//________________________//
 
 class App extends React.Component {
   state = {
